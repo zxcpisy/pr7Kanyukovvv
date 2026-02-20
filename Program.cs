7e70b88b457c7fcaabb0e4bcdfbfe73e8d6e44c7
Tashikv <vozhakovatasha1@gmail.com>
2026-02-20 10:30:55 +0500

    Получение информации с сайта.Kanyukov

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,17 @@ namespace pr7Kan
     {
         static void Main(string[] args)
         {
-        }
+            WebRequest request = WebRequest.Create("");
+            HttpWebResponse responce = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine(responce.StatusDescription);
+            Stream dataStream = responce.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responceFromServer = reader.ReadToEnd();
+            Console.WriteLine(responceFromServer);
+            reader.Close();
+            dataStream.Close();
+            responce.Close();
+            Console.Read();
+         }
     }
 }
